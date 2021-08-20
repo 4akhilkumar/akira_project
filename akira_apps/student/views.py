16a2ae15dc02d5ec07920ff98116a0a0ac2f99f9
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:31:40 +0530

    Student Dashboard

--- akira_apps/student/views.py
+++ akira_apps/student/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from akira_apps.authentication.models import UserLoginDetails
 
-# Create your views here.
+@login_required
+def student_dashboard(request):
+    previous_user_login_details = UserLoginDetails.objects.filter(user__username = request.user)
+    context = {
+        "previous_user_login_details":previous_user_login_details,
+    }
+    return render(request, 'student/dashboard.html', context)
