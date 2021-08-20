462e7bc33e51c65640b55912dd119993d12bdd52
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:26:40 +0530

    Model Classes Registered [Faculty]

--- akira_apps/faculty/admin.py
+++ akira_apps/faculty/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Staffs, Course,CourseRegistration
 
-# Register your models here.
+admin.site.register(Staffs)
+admin.site.register(Course)
+admin.site.register(CourseRegistration)
