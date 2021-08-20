2d0ed3434aea3cdd6708eb4e09d0daa582deb994
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:35:22 +0530

    Update views.py
    super_admin_dashboard
    add_staff
    manage_staff
    view_staff

--- akira_apps/super_admin/views.py
+++ akira_apps/super_admin/views.py
@@ -1,3 +1,71 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User, Group
+from django.contrib.auth import authenticate
+from django.contrib.auth.decorators import login_required
+from django.conf import settings
 
-# Create your views here.
+from akira_apps.faculty.forms import CreateUserForm, StaffsForm
+from akira_apps.faculty.models import Staffs
+
+@login_required(login_url=settings.LOGIN_URL)
+def super_admin_dashboard(request):
+    return render(request, 'super_admin/dashboard.html')
+
+@login_required(login_url=settings.LOGIN_URL)
+def add_staff(request):
+    form = CreateUserForm()
+    staff_form = StaffsForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        staff_form = StaffsForm(request.POST,request.FILES)
+        staff_from_db = User.objects.all()
+        staff_user=[]
+        for i in staff_from_db:
+            staff_user.append(i.username)
+            staff_user.append(i.email)
+        username = request.POST.get('username')
+        if username in staff_user:
+            print("A user already exist with "+ username)
+            return redirect('add_staff')
+
+        if form.is_valid() and staff_form.is_valid():
+            user = form.save()
+            staff = staff_form.save(commit=False)
+            staff.user = user
+            staff.save()
+
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            
+            user = authenticate(username = username, password = password)
+
+            group = Group.objects.get(name='Faculty')
+            user.groups.add(group)
+            print("Faculty Registered Successfully.")
+            return redirect('manage_staff')
+        else:
+            form = CreateUserForm()
+            staff_form = StaffsForm()
+
+    context = {
+        'form':form,
+        'staff_form':staff_form
+    }       
+    return render(request, 'super_admin/Staff/add_faculty.html', context)
+
+@login_required(login_url=settings.LOGIN_URL)
+def manage_staff(request):
+    staffs = Staffs.objects.all()
+    context = {
+        "staffs":staffs
+    }
+    return render(request, 'super_admin/Staff/manage_faculty.html', context)
+
+@login_required(login_url=settings.LOGIN_URL)
+def view_staff(request, staff_id):
+    staff = Staffs.objects.get(user=staff_id)
+    context = {
+        "staff": staff,
+        "id": staff_id,
+    }
+    return render(request, "super_admin/Staff/view_faculty.html", context)
