b42c42b90c9fafc2866f7c8b36eb52929a584066
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:19:07 +0530

    SMTP (Email) Configuration

--- akira/settings.py
+++ akira/settings.py
@@ -125,6 +125,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+
+#SMTP Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '4projtest@gmail.com'
+EMAIL_HOST_PASSWORD = 'qwypiocbcstwltgk'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
