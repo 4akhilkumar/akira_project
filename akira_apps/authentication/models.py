7f766bdd40047503701f52f0026fd17bb91e61fa
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:24:19 +0530

    UserLoginDetails Model Class

--- akira_apps/authentication/models.py
+++ akira_apps/authentication/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
-# Create your models here.
+class UserLoginDetails(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    user_ip_address = models.CharField(max_length=100)
+    os_details = models.CharField(max_length=100)
+    browser_details = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.user, self.user_ip_address)
