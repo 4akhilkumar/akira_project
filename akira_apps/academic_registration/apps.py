05a0ea938d7a2d35738206a5d4a31830bcd8e578
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-08-20 19:22:17 +0530

    Add path i.e., akira_apps in name

---
+++ akira_apps/academic_registration/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AcademicRegistrationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'akira_apps.academic_registration' # Add path i.e., akira_apps in name
