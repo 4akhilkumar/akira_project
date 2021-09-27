88aed13bb367609378e46a0888c5e012b538db49
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-09-27 09:10:37 +0530

    Deleted - login.css, navbar.css, dashboard.html and Faculty App

--- akira_apps/faculty/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Staffs, Course,CourseRegistration
-
-admin.site.register(Staffs)
-admin.site.register(Course)
-admin.site.register(CourseRegistration)
