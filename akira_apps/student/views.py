a54f87bac07a7d9464d30e27531c2174aa4f1cfc
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-09-27 09:30:47 +0530

    Update views.py

--- akira_apps/student/views.py
+++ akira_apps/student/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 from akira_apps.authentication.models import UserLoginDetails
 
 import secrets
+
 @login_required
 def student_dashboard(request):
     previous_user_login_details = UserLoginDetails.objects.filter(user__username = request.user)
     rAnd0m123 = secrets.token_urlsafe(16)
-    print(rAnd0m123)
     context = {
         "previous_user_login_details":previous_user_login_details,
         "rAnd0m123":rAnd0m123,
