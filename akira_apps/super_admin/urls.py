b6d5ddd6f946b6462396485ef1fe11504e9d5be5
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-12 09:09:29 +0530

    Staff (Create & View), User Group Manage - Super_Admin

--- akira_apps/super_admin/urls.py
+++ akira_apps/super_admin/urls.py
@@ -3,7 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.super_admin_dashboard, name = 'super_admin_dashboard'),
-    path('add_staff/', views.add_staff, name="add_staff"),
-    path('manage_staff/', views.manage_staff, name="manage_staff"),
-    path('view_staff/<staff_id>/', views.view_staff, name="view_staff"),
+    path('my_profile/', views.my_profile, name = 'my_profile'),
+    path('save_my_profile/', views.save_my_profile, name = 'save_my_profile'),
+    path('assign_group/', views.assign_group, name = 'assign_group'),
+    path('add_staff/', views.add_staff, name = 'add_staff'),
+    path('manage_staff/', views.manage_staff, name = 'manage_staff'),
+    path('view_staff/<staff_id>/', views.view_staff, name = 'view_staff'),
+    path('user_group/<staff_id>/', views.user_group, name = 'user_group'),
 ]
