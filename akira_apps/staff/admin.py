8cb5826423e0edc4674fcd5b83430793f5d19b70
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-12 08:51:37 +0530

    from akira_apps.staff.models import Staffs, Course_Sessions, CourseOutcome, CourseSession, CourseALMS, ALM

--- akira_apps/staff/admin.py
+++ akira_apps/staff/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Staffs, Course,CourseRegistration
+from akira_apps.staff.models import Staffs, Course_Sessions, CourseOutcome, CourseSession, CourseALMS, ALM
 
 admin.site.register(Staffs)
-admin.site.register(Course)
-admin.site.register(CourseRegistration)
+admin.site.register(Course_Sessions)
+admin.site.register(CourseOutcome)
+admin.site.register(CourseSession)
+admin.site.register(CourseALMS)
+admin.site.register(ALM)
