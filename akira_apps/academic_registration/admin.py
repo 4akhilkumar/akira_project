ff43557683f450d44d51cd037992255b083569d4
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-12 08:46:45 +0530

    from akira_apps.academic_registration.models import classes
    import Block, SectionRooms, Semester, academic_registration_staff, academic_registration_student, Specialization, specialization_registration, Course, course_registration_student, course_registration_staff

--- akira_apps/academic_registration/admin.py
+++ akira_apps/academic_registration/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import *
+from akira_apps.academic_registration.models import Block, SectionRooms, Semester, academic_registration_staff, academic_registration_student, Specialization, specialization_registration, Course, course_registration_student, course_registration_staff
 
-admin.site.register(SectionRooms)
 admin.site.register(Block)
-admin.site.register(Branch)
+admin.site.register(SectionRooms)
 admin.site.register(Semester)
+admin.site.register(academic_registration_staff)
+admin.site.register(academic_registration_student)
+admin.site.register(Specialization)
+admin.site.register(specialization_registration)
+admin.site.register(Course)
+admin.site.register(course_registration_student)
+admin.site.register(course_registration_staff)
