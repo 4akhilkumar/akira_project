95f6df8e34fb6352a8cf8ed945bf9c69102531a0
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-14 08:31:42 +0530

    fetch course_registration_student from academic_registration

--- akira_apps/student/admin.py
+++ akira_apps/student/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Students
+from .models import Students, course_registration_student
 
 admin.site.register(Students)
+admin.site.register(course_registration_student)
