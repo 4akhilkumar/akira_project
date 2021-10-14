80c2090fd1bd46c1a640a5207dc0c58889d655c8
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-14 08:02:16 +0530

    Carousel Code Moved into a New File

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -103,22 +103,6 @@ themeToggle.addEventListener('click', () => {
   }
 });
 
-/* ======== Carousel ======== */
-var slideIndex = 0;
-carousel();
-
-function carousel() {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none"; 
-  }
-  slideIndex++;
-  if (slideIndex > x.length) {slideIndex = 1} 
-  x[slideIndex-1].style.display = "block"; 
-  setTimeout(carousel, 4000); 
-}
-
 /* ======= checkSemesterStatus ======= */
 function checkSemesterStatus() {
   var checkBox = document.getElementById("id_semester_status");
