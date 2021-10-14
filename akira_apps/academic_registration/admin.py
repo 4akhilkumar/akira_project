f59e1e1ad26c5174bae30f9a4594c54fd0a9a7e0
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-10-14 08:04:41 +0530

    specialization_registration -> specialization_registration_staff, specialization_registration_student
    course_registration_student is moved into a another admin.py (Student)

--- akira_apps/academic_registration/admin.py
+++ akira_apps/academic_registration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from akira_apps.academic_registration.models import Block, SectionRooms, Semester, academic_registration_staff, academic_registration_student, Specialization, specialization_registration, Course, course_registration_student, course_registration_staff
+from akira_apps.academic_registration.models import Block, SectionRooms, Semester, academic_registration_staff, academic_registration_student, Specialization, specialization_registration_staff, specialization_registration_student, Course, course_registration_staff
 
 admin.site.register(Block)
 admin.site.register(SectionRooms)
@@ -7,7 +7,7 @@ admin.site.register(Semester)
 admin.site.register(academic_registration_staff)
 admin.site.register(academic_registration_student)
 admin.site.register(Specialization)
-admin.site.register(specialization_registration)
+admin.site.register(specialization_registration_staff)
+admin.site.register(specialization_registration_student)
 admin.site.register(Course)
-admin.site.register(course_registration_student)
 admin.site.register(course_registration_staff)
