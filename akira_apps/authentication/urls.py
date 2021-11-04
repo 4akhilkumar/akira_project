a17cebac69c96509d2334b48831f853a186c730f
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-04 16:56:35 +0530

    reset_password

--- akira_apps/authentication/urls.py
+++ akira_apps/authentication/urls.py
@@ -10,4 +10,19 @@ urlpatterns = [
     path('logout/', views.logoutUser, name="logout"),
     
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+    
+    path('reset_password/',
+        auth_views.PasswordResetView.as_view(template_name="authentication/password_reset/password_reset.html", 
+                                            html_email_template_name = 'authentication/password_reset/password_reset_email.html',
+                                            subject_template_name = 'authentication/password_reset/password_reset_subject.txt'),
+        name="reset_password"),
+    path('reset_password_sent/',
+        auth_views.PasswordResetDoneView.as_view(template_name="authentication/password_reset/password_reset_sent.html"),
+        name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name="authentication/password_reset/password_reset_form.html"),
+        name="password_reset_confirm"),
+    path('reset_password_complete/',
+        auth_views.PasswordResetCompleteView.as_view(template_name="authentication/password_reset/password_reset_complete.html"),
+        name="password_reset_complete"),
 ]
