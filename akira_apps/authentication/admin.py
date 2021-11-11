761c831ae79b0ab362234c7bb3ae64bfda52ceec
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-11 11:59:59 +0530

    UserVerificationStatus model class created

--- akira_apps/authentication/admin.py
+++ akira_apps/authentication/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserLoginDetails, User_IP_B_List
+from .models import UserLoginDetails, User_IP_B_List, UserVerificationStatus
 
 admin.site.register(UserLoginDetails)
 admin.site.register(User_IP_B_List)
+admin.site.register(UserVerificationStatus)
--- akira_apps/authentication/models.py
+++ akira_apps/authentication/models.py
@@ -16,7 +16,20 @@ class UserLoginDetails(models.Model):
         return '%s - %s' % (self.user, self.user_ip_address)
 
     class Meta:
-        ordering = ['-created_at']
+        ordering = ['created_at']
+
+class UserVerificationStatus(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.user, self.status)
+
+    class Meta:
+        ordering = ['created_at']
+
 
 class User_IP_B_List(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
