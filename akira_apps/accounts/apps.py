eb27654fc77897afc50100ce5c8a692c15b8d330
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-15 09:11:57 +0530

    Add path i.e., akira_apps in name

---
+++ akira_apps/accounts/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AccountsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'akira_apps.accounts' # Add path i.e., akira_apps in name
