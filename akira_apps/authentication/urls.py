87d77eb13fc9817e94dc02e560a12e95b143b3f7
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-16 07:13:12 +0530

    twofacauth urls

--- akira_apps/authentication/urls.py
+++ akira_apps/authentication/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import activate, confirm
+from .views import activate, confirm, twofacauth
 from django.contrib.auth import views as auth_views
 from akira_apps import authentication
 
@@ -16,6 +16,11 @@ urlpatterns = [
     path('confirm/<uidb64>/<token>/', confirm, name='confirm'),
     
     path('activate/<uidb64>/<token>/', activate, name='activate'),
+
+    path('twofa_verify_its_you/<username>/', views.twofa_verify_its_you, name="twofa_verify_its_you"),
+    path('twofa_verify_user_by_email/<username>/', views.twofa_verify_user_by_email, name="twofa_verify_user_by_email"),
+    path('twofacauth/<uidb64>/<token>/', twofacauth, name='twofacauth'),
+    path('twofa_verify_user_by_backup_codes/<username>/', views.twofa_verify_user_by_backup_codes, name="twofa_verify_user_by_backup_codes"),
     
     path('reset_password/',
         auth_views.PasswordResetView.as_view(template_name="authentication/password_reset/password_reset.html", 
