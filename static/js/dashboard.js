6766476ecb375bb7a6df418e1a02f2687f451763
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-11-21 16:47:16 +0530

    top-button

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -46,3 +46,21 @@ if (darkMode !== 'enabled') {
     disableDarkMode(); 
 }
 });
+// ====================== End Dark Mode ====================== //
+
+// ====================== Top Button ====================== //
+var mybutton = document.getElementById("myTopButton");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+// ====================== End Top Button ====================== //
