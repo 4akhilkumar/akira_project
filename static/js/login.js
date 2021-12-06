b0d195253f805e602446a3e1b11b6d03c3273631
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-06 23:07:08 +0530

    disable button submit the form

--- static/js/login.js
+++ static/js/login.js
@@ -46,3 +46,8 @@ $(".toggle-password").click(function() {
 //         submitButton.disabled = true;
 //     }
 // }
+
+$('.app-login-button').click(function() {
+    $(this).attr('disabled', true);
+    $(this).closest('form').submit();
+});
