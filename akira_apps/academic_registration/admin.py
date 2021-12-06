36d1811015b336c385bd820ff4dee762cb71a6c9
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-06 10:55:50 +0530

    removed and exported to academic app
    exported = [Block, SectionRooms]

--- akira_apps/academic_registration/admin.py
+++ akira_apps/academic_registration/admin.py
@@ -1,13 +1,1 @@
 from django.contrib import admin
-from akira_apps.academic_registration.models import Block, SectionRooms, Semester, academic_registration_staff, academic_registration_student, Specialization, specialization_registration_staff, specialization_registration_student, Course, course_registration_staff
-
-admin.site.register(Block)
-admin.site.register(SectionRooms)
-admin.site.register(Semester)
-admin.site.register(academic_registration_staff)
-admin.site.register(academic_registration_student)
-admin.site.register(Specialization)
-admin.site.register(specialization_registration_staff)
-admin.site.register(specialization_registration_student)
-admin.site.register(Course)
-admin.site.register(course_registration_staff)
