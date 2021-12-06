2ca4380f34d7cd0c92465e6d59c3519d18b82601
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-06 11:58:13 +0530

    data fetching url is changed due to connection error where internet firewall is installed

--- akira_apps/staff/templatetags/ip_location.py
+++ akira_apps/staff/templatetags/ip_location.py
@@ -5,14 +5,17 @@ import requests
 
 @register.filter
 def ip_location(value):
-    url = 'http://ip-api.com/json/{}'.format(value)
-    response = requests.get(url)
-    data = response.json()
-    if data['status'] == 'success':
-        city_region_country = data['city'] + ', ' + data['regionName'] + ', ' + data['country']
-    elif data['status'] == 'fail':
-        if data['query'] == '127.0.0.1':
-            city_region_country = 'Localhost'
+    try:
+        url = 'https://ipinfo.io/{}/geo'.format(value)
+        response = requests.get(url)
+        data = response.json()
+        if data['bogon'] == True:
+            if data['ip'] == '127.0.0.1':
+                city_region_country = 'Localhost'
+            else:
+                city_region_country = 'Unknown'
         else:
-            city_region_country = 'Unknown'
-    return city_region_country
+            city_region_country = data['city'] + ', ' + data['region'] + ', ' + data['country']
+        return city_region_country
+    except Exception as e:
+        return value
