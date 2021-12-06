8131a3c9ac66f0521b537c4f12279e365f584c5b
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-06 23:06:42 +0530

    Bug Fixed - Fetching Location via IP Address API

--- akira_apps/staff/templatetags/ip_location.py
+++ akira_apps/staff/templatetags/ip_location.py
@@ -6,14 +6,13 @@ import requests
 @register.filter
 def ip_location(value):
     try:
-        url = 'https://ipinfo.io/{}/geo'.format(value)
+        # url = 'https://ipinfo.io/{}/geo'.format(value)
+        url = 'https://ipapi.co/{}/json/'.format(value)
         response = requests.get(url)
         data = response.json()
-        if data['bogon'] == True:
-            if data['ip'] == '127.0.0.1':
+        if (data['ip'] == '127.0.0.1'):
+            if (data['error'] == True) or (data['reserved'] == True):
                 city_region_country = 'Localhost'
-            else:
-                city_region_country = 'Unknown'
         else:
             city_region_country = data['city'] + ', ' + data['region'] + ', ' + data['country']
         return city_region_country
