7fd49544c80ffbf98d073330cab29a756ac2b7a1
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-12 18:13:21 +0530

    Update urls.py

--- akira_apps/super_admin/urls.py
+++ akira_apps/super_admin/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.super_admin_dashboard, name = 'super_admin_dashboard'),
+    path('super_admin_dashboard/', views.super_admin_dashboard, name = 'super_admin_dashboard'),
     path('my_profile/', views.my_profile, name = 'my_profile'),
     path('save_my_profile/', views.save_my_profile, name = 'save_my_profile'),
     path('assign_group/', views.assign_group, name = 'assign_group'),
