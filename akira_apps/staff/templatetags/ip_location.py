35f454af4011f0e0f3e03a0c8f885dc0b8569510
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-12 18:10:51 +0530

    IP API Components Updated

--- akira_apps/staff/templatetags/ip_location.py
+++ akira_apps/staff/templatetags/ip_location.py
@@ -6,12 +6,11 @@ import requests
 @register.filter
 def ip_location(value):
     try:
-        # url = 'https://ipinfo.io/{}/geo'.format(value)
-        url = 'https://ipapi.co/{}/json/'.format(value)
+        url = 'https://ipinfo.io/{}/geo'.format(value)
         response = requests.get(url)
         data = response.json()
         if (data['ip'] == '127.0.0.1'):
-            if (data['error'] == True) or (data['reserved'] == True):
+            if (data['bogon'] == True):
                 city_region_country = 'Localhost'
         else:
             city_region_country = data['city'] + ', ' + data['region'] + ', ' + data['country']
