c9312f0fcbfe0c9a9f2e7bed3f7ed7b45cb00cbf
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-12 17:56:33 +0530

    Semester Form, Branch Form removed

--- akira_apps/academic_registration/forms.py
+++ akira_apps/academic_registration/forms.py
@@ -1,21 +1,6 @@
 from django import forms
 from django.db import models
 
-from akira_apps.academic_registration.models import BRANCH_CHOICES, Semester, Course
-
-class SemesterForm(forms.Form):
-    SEM_MODE = (
-        ('ODD','ODD'),
-        ('EVEN','EVEN'),
-    )
-    model = Semester
-    mode = forms.ChoiceField(choices = SEM_MODE)
-    fields = ['mode']
-
-class BranchForm(forms.Form):
-    branch = forms.ChoiceField(choices = BRANCH_CHOICES)
-    fields = ['branch']
-
 class SectionRoomForm(forms.Form):
     class Floor(models.TextChoices):
         Ground = 'Ground'
