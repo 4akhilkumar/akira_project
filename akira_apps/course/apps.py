da64b74fc716994f27a088f542aa0ca01eec239b
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-12 18:06:33 +0530

    course app admin, apps

---
+++ akira_apps/course/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CourseConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'akira_apps.course' # Add path i.e., akira_apps in name
