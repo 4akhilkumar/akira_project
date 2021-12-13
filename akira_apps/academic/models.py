1c0f803609059cd812c88857d480b23ce85c2747
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-13 11:21:11 +0530

    description field properties modified

--- akira_apps/academic/models.py
+++ akira_apps/academic/models.py
@@ -4,7 +4,7 @@ import uuid
 class Block(models.Model):
     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
     block_name = models.CharField(max_length = 50, unique = True)
-    block_desc = models.CharField(max_length = 100)
+    block_desc = models.CharField(max_length = 100, blank = True, null = True)
 
     def __str__(self):
         return '%s' % (self.block_name)
