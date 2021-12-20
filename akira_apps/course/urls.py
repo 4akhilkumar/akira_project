53e83ecfbac8a693814cbc2f20fef1939f5ff9c4
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-20 15:15:37 +0530

    search_course url path

--- akira_apps/course/urls.py
+++ akira_apps/course/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('manage_courses/', views.manage_courses, name='manage_courses'),
-    path('create_course/', views.create_course, name='create_course'),
     path('create_course_save/', views.create_course_save, name='create_course_save'),
     path('view_course/<course_code>/', views.view_course, name='view_course'),
+    path('search_course/', views.search_course, name='search_course'),
     path('delete_course/<course_id>/', views.delete_course, name='delete_course'),
 ]
