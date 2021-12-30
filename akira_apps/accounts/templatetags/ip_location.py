4134ddac1b7da1b6458b196465647e9f89e676e9
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-30 11:03:47 +0530

    Create ip_location.py

---
+++ akira_apps/accounts/templatetags/ip_location.py
@@ -0,0 +1,19 @@
+from django import template
+register = template.Library()
+
+import requests
+
+@register.filter
+def ip_location(value):
+    try:
+        url = 'https://ipinfo.io/{}/geo'.format(value)
+        response = requests.get(url)
+        data = response.json()
+        if (data['ip'] == '127.0.0.1'):
+            if (data['bogon'] == True):
+                city_region_country = 'Localhost'
+        else:
+            city_region_country = data['city'] + ', ' + data['region'] + ', ' + data['country']
+        return city_region_country
+    except Exception as e:
+        return value
