e69e4fb054414b7c3b518267e650e332c7630bba
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-30 10:55:09 +0530

    delete_resource

--- akira_apps/shigen/urls.py
+++ akira_apps/shigen/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('resource/', views.manage_resources, name = 'manage_resources'),
     path('create_resource_save/', views.create_resource_save, name = 'create_resource_save'),
     path('view_resource/<resource_id>/', views.view_resource, name = 'view_resource'),
+    path('delete_resource/<resource_id>/', views.delete_resource, name = 'delete_resource'),
 ]
