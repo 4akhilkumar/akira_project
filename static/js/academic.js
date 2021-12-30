6be72dda76123ea759224b93a50919a8b5911c9b
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-30 11:01:48 +0530

    Create academic.js

---
+++ static/js/academic.js
@@ -0,0 +1,53 @@
+var modal = document.getElementById("myModal");
+var modal2 = document.getElementById("myModal2");
+var modal3 = document.getElementById("myModal3");
+var modal4 = document.getElementById("myModal4");
+var btn = document.getElementById("showFormCreateBlock");
+var btn2 = document.getElementById("showFormCreateFloor");
+var btn3 = document.getElementById("showFormCreateRoom");
+var btn4 = document.getElementById("showFormCreateSemester");
+var span = document.getElementById("close-model");
+var span2 = document.getElementById("close-model2");
+var span3 = document.getElementById("close-model3");
+var span4 = document.getElementById("close-model4");
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
+btn3.onclick = function() {
+  modal3.style.display = "block";
+}
+btn4.onclick = function() {
+  modal4.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+span2.onclick = function() {
+  modal2.style.display = "none";
+}
+span3.onclick = function() {
+  modal3.style.display = "none";
+}
+span4.onclick = function() {
+  modal4.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+  if (event.target == modal2) {
+    modal2.style.display = "none";
+  }
+  if (event.target == modal3) {
+    modal3.style.display = "none";
+  }
+  if (event.target == modal4) {
+    modal4.style.display = "none";
+  }
+}
