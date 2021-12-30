6d530f3e47b0aa48576612b2526e71bba10c0f0a
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2021-12-30 10:47:46 +0530

    Update models.py
    For clashes Course->CourseMC,
    specialization field is added to CourseMC,
    Delete the media file in local repo if the object is deleted in DB,

--- akira_apps/course/models.py
+++ akira_apps/course/models.py
@@ -1,11 +1,14 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.dispatch import receiver
 
 import uuid
+import os
 
 from akira_apps.academic.models import (Semester)
+from akira_apps.specialization.models import (SpecializationsMC)
 
-class Course(models.Model):
+class CourseMC(models.Model):
     BRANCH_CHOICES = [
         ("","Branch Name"),
         ("Computer Science and Engineering","Computer Science and Engineering"),
@@ -28,13 +31,66 @@ class Course(models.Model):
     course_coordinator = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     branch = models.CharField(max_length = 50, choices = BRANCH_CHOICES, default=1)
     semester = models.ForeignKey(Semester, on_delete=models.SET_NULL, blank=True, null=True)
+    specialization = models.ForeignKey(SpecializationsMC, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self):
         return '%s - %s' % (self.course_code, self.course_name)
 
 class CourseFiles(models.Model):
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    course_files = models.FileField(upload_to='Course Files/', blank=True, null=True)
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    course = models.ForeignKey(CourseMC, on_delete=models.CASCADE)
+    course_files = models.FileField(upload_to='Course_Files/', blank=True, null=True)
 
     def __str__(self):
         return '%s - %s' % (self.course, self.course_files)
+
+@receiver(models.signals.post_delete, sender=CourseFiles)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.course_files:
+        if os.path.isfile(instance.course_files.path):
+            try:
+                os.remove(instance.course_files.path)
+            except Exception:
+                os.unlink(instance.course_files.path)
+
+class CourseComponent(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    course = models.ForeignKey(CourseMC, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    desc = models.TextField(max_length = 500, null=True, blank=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.course, self.name)
+
+class CourseSubComponent(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    course = models.ForeignKey(CourseMC, on_delete=models.CASCADE)
+    component = models.ForeignKey(CourseComponent, on_delete=models.CASCADE)
+    name = models.CharField(max_length = 100)
+    desc = models.TextField(max_length = 500, null=True, blank=True)
+
+    def __str__(self):
+        return '%s - %s' % (self.course, self.name)
+
+class CourseTask(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    course = models.ForeignKey(CourseMC, on_delete=models.CASCADE)
+    component = models.ForeignKey(CourseComponent, on_delete=models.CASCADE)
+    sub_component = models.ForeignKey(CourseSubComponent, on_delete=models.CASCADE)
+    question = models.TextField(max_length = 500)
+    answer = models.ForeignKey('TaskAnswer', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '%s' % (self.question)
+
+class TaskAnswer(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(CourseMC, on_delete=models.CASCADE)
+    component = models.ForeignKey(CourseComponent, on_delete=models.CASCADE)
+    sub_component = models.ForeignKey(CourseSubComponent, on_delete=models.CASCADE)
+    task = models.ForeignKey(CourseTask, on_delete=models.CASCADE)
+    answer = models.FileField(upload_to='TaskAnswerFiles/')
+
+    def __str__(self):
+        return '%s - %s' % (self.user, self.answer)
