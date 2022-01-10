f3b62c50e5b76d9660033d50f75398c2ca503130
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-01-10 09:27:36 +0530

    removed the static IP Address condition

--- akira_apps/accounts/templatetags/ip_location.py
+++ akira_apps/accounts/templatetags/ip_location.py
@@ -9,9 +9,8 @@ def ip_location(value):
         url = 'https://ipinfo.io/{}/geo'.format(value)
         response = requests.get(url)
         data = response.json()
-        if (data['ip'] == '127.0.0.1'):
-            if (data['bogon'] == True):
-                city_region_country = 'Localhost'
+        if (data['bogon'] == True):
+            city_region_country = 'Unknown'
         else:
             city_region_country = data['city'] + ', ' + data['region'] + ', ' + data['country']
         return city_region_country
