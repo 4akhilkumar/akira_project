a35deb679263dce5b152d35cdddec305fa48db59
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-01-16 17:22:51 +0530

    default = False

--- akira_apps/academic/models.py
+++ akira_apps/academic/models.py
@@ -70,7 +70,7 @@ class Semester(models.Model):
     start_year = models.DateField()
     end_year = models.DateField()
     branch = models.CharField(max_length = 50, choices = BRANCH_CHOICES, default=1)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s %s' % (self.mode, self.start_year.year)
