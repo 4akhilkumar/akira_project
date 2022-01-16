8ac4af3bd10fcac967c769608e7986dd58e348cf
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-01-16 16:55:51 +0530

    semester is remove in academic and added in semRegistration

--- akira_apps/academic_registration/urls.py
+++ akira_apps/academic_registration/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('sem_registration/', views.sem_registration, name='sem_registration'),
     path('enrollSpec/<uuid:speci_id>/', views.enrollSpec, name='enrollSpec'),
     path('unenrollSpec/<uuid:speci_id>/', views.unenrollSpec, name='unenrollSpec'),
+    path('create_semester_save/', views.create_semester_save, name='create_semester_save'),
+    path('fetch_semester/<semester_id>/', views.fetch_semester, name='fetch_semester'),
+    path('update_semester_save/<semester_id>/', views.update_semester_save, name='update_semester_save'),
+    path('delete_semester/<semester_id>/', views.delete_semester, name='delete_semester'),
 ]
