55560a8140d4049c1873feca4884a1d8b12826da
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-03-12 23:58:40 +0530

    MailLog modal class

--- akira_apps/super_admin/models.py
+++ akira_apps/super_admin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import uuid
+
+class MailLog(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    subject = models.TextField(max_length = 1024)
+    created_at = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return '%s - %s' % (self.user, self.subject)
+    
+    class Meta:
+        ordering = ["created_at"]
