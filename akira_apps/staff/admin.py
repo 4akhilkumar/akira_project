adfcefeee0cd9bc620a176946ad0a03a2af368d2
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-03-14 19:53:19 +0530

    about, skill fields are added to Staff Modal Class

--- akira_apps/staff/admin.py
+++ akira_apps/staff/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-from akira_apps.staff.models import Staff
+from akira_apps.staff.models import (Staff, Skills)
+
 admin.site.register(Staff)
+admin.site.register(Skills)
--- akira_apps/staff/models.py
+++ akira_apps/staff/models.py
@@ -67,6 +67,8 @@ class Staff(models.Model):
     country = models.CharField(max_length=50)
     photo = models.ImageField(null=True, blank=True, upload_to='staff_photos/', validators=[FileExtensionValidator(['jpg', 'png', 'jpeg'])])
     branch = models.ForeignKey(Branch, on_delete = models.DO_NOTHING, blank = True, null = True)
+    about = models.TextField(max_length=500, null=True, blank=True)
+    skills = models.ManyToManyField('Skills', blank = True)
     resume = models.FileField(upload_to ='staff_resumes/', validators=[FileExtensionValidator(['pdf'])], blank = True, null = True)
 
     def __str__(self):
@@ -74,3 +76,19 @@ class Staff(models.Model):
     
     class Meta:
         ordering = ["user__first_name"]
+
+class Skills(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    name = models.CharField(max_length = 50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+# class UserExtraFields(models.Model):
+#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+#     staffMC = models.ForeignKey(Staff, on_delete = models.CASCADE)
+#     field_name = models.CharField(max_length = 100)
+#     field_value = models.TextField(max_length=500)
+
+#     def __str__(self):
+#         return '%s' % (self.field_name)
