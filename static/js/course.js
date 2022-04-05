307a43daa9c4cb16750acb10e70d86a8364dca78
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-04-05 14:52:44 +0530

    Course button validation is added

--- static/js/course.js
+++ static/js/course.js
@@ -1,6 +1,6 @@
-$('.course-btn').click(function() {
-    $(this).attr('disabled', true);
-    $(this).val('Please wait...');
+$('#course-btn').click(function() {
+    $(this).prop('disabled', true);
+    $(this).html('Please wait...');
     $(this).closest('form').submit();
 });
 
