826ce644a45bd059e3691a465d90a0789a2e5ee6
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-04-22 15:36:22 +0530

    Validation is done

--- static/js/modify_course.js
+++ static/js/modify_course.js
@@ -480,14 +480,26 @@ $(document).ready(function() {
         previousFieldSet.call(this);
     });
     
-    // $("input[data-create-design='true']").prop('disabled', true);
-    // $("#course-btn").prop("disabled", true);
+    $("input[data-create-design='true']").prop('disabled', true);
+    $("#course-btn").prop("disabled", true);
     var course_btn = false;
 
     var code = false; var name = false;
     var description = false;
     var branch = false; var semester = false;
     var faculty = false; var course_type = false;
+
+    if ($("#id_course_code").val() == "") {
+        $("#id_course_code").parent().find(".error-text").css("display", "block");
+        code = false;
+    } else if (!/^[A-Za-z0-9-\s]*$/.test($("#id_course_code").val())) {
+        $("#id_course_code").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the course code correctly&#x0003F;");
+        $("#id_course_code").parent().find(".error-text").css("display", "block");
+        code = false;
+    } else {
+        $("#id_course_code").parent().find(".error-text").css("display", "none");
+        code = true;
+    }
 
     $('#id_course_code').on('keyup keydown blur change', function() {
         if ($("#id_course_code").val() == "") {
@@ -502,6 +514,26 @@ $(document).ready(function() {
             code = true;
         }
     });
+
+    if($("#id_course_name").val() == "") {
+        $("#id_course_name").parent().find(".error-text").html("Enter the course name");
+        $("#id_course_name").parent().find(".error-text").css("display", "block");
+        name = false;
+    }
+    else if (!$("#id_course_name").val().match(/^[A-Za-z&\-\s]*$/)) {
+        $("#id_course_name").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the course name correctly&#x0003F;");
+        $("#id_course_name").parent().find(".error-text").css("display", "block");
+        name = false;
+    }
+    else if ($("#id_course_name").val().match(/^\s+$/)) {
+        $("#id_course_name").parent().find(".error-text").html("Sorry, but the course name cannot be empty");
+        $("#id_course_name").parent().find(".error-text").css("display", "block");
+        name = false;
+    }
+    else {
+        $("#id_course_name").parent().find(".error-text").css("display", "none");
+        name = true;
+    }
 
     $('#id_course_name').on('keyup keydown blur change', function() {
         if($("#id_course_name").val() == "") {
@@ -525,6 +557,16 @@ $(document).ready(function() {
         }
     });
 
+    if (!$("#id_course_desc").val().match(/^\s+$/)) {
+        var chars = $('#id_course_desc').val().length;
+        var max = $('#id_course_desc').attr('maxlength');
+        var remaining = max - chars;
+        var char_text = remaining == 1 ? 'character' : 'characters';
+        $('#id_course_desc').parent().find(".help-text").css("display", "none");
+        $('#id_course_desc').parent().find(".info-text").html("You have " + remaining + " " + char_text + " remaining");
+        $('#id_course_desc').parent().find(".info-text").css("display", "block");
+    }
+
     $('#id_course_desc').on('keyup keydown change', function() {
         if (!$("#id_course_desc").val().match(/^\s+$/)) {
             var chars = $('#id_course_desc').val().length;
@@ -536,6 +578,26 @@ $(document).ready(function() {
             $('#id_course_desc').parent().find(".info-text").css("display", "block");
         }
     });
+
+    if($("#id_course_desc").val() == "") {
+        $("#id_course_desc").parent().find(".error-text").html("Enter the course description");
+        $("#id_course_desc").parent().find(".error-text").css("display", "block");
+        description = false;
+    }
+    else if ($("#id_course_desc").val().match(/^\s+$/)) {
+        $("#id_course_desc").parent().find(".error-text").html("Sorry, but the course description cannot be empty");
+        $("#id_course_desc").parent().find(".error-text").css("display", "block");
+        description = false;
+    }        
+    else if ($("#id_course_desc").val().length > 500) {
+        $("#id_course_desc").parent().find(".error-text").html("Sorry, but the course description cannot be more than 500 characters");
+        $("#id_course_desc").parent().find(".error-text").css("display", "block");
+        description = false;
+    }
+    else {
+        $("#id_course_desc").parent().find(".error-text").css("display", "none");
+        description = true;
+    }
 
     $('#id_course_desc').on('keyup keydown blur change', function() {
         if($("#id_course_desc").val() == "") {
@@ -559,6 +621,14 @@ $(document).ready(function() {
         }
     });
 
+    if ($("#id_branch").val() == "") {
+        $("#id_branch").parent().find(".error-text").css("display", "block");
+        branch = false;
+    } else {
+        $("#id_branch").parent().find(".error-text").css("display", "none");
+        branch = true;
+    }
+
     $('#id_branch').on('keyup keydown blur change', function() {
         if ($("#id_branch").val() == "") {
             $("#id_branch").parent().find(".error-text").css("display", "block");
@@ -568,6 +638,14 @@ $(document).ready(function() {
             branch = true;
         }
     });
+
+    if ($("#id_semester").val() == "") {
+        $("#id_semester").parent().find(".error-text").css("display", "block");
+        semester = false;
+    } else {
+        $("#id_semester").parent().find(".error-text").css("display", "none");
+        semester = true;
+    }
 
     $('#id_semester').on('keyup keydown blur change', function() {
         if ($("#id_semester").val() == "") {
@@ -579,6 +657,14 @@ $(document).ready(function() {
         }
     });
 
+    if ($("#id_course_coordinator").val() == "") {
+        $("#id_course_coordinator").parent().find(".error-text").css("display", "block");
+        faculty = false;
+    } else {
+        $("#id_course_coordinator").parent().find(".error-text").css("display", "none");
+        faculty = true;
+    }
+
     $('#id_course_coordinator').on('keyup keydown blur change', function() {
         if ($("#id_course_coordinator").val() == "") {
             $("#id_course_coordinator").parent().find(".error-text").css("display", "block");
@@ -589,6 +675,14 @@ $(document).ready(function() {
         }
     });
 
+    if ($("#id_course_type").val() == "") {
+        $("#id_course_type").parent().find(".error-text").css("display", "block");
+        course_type = false;
+    } else {
+        $("#id_course_type").parent().find(".error-text").css("display", "none");
+        course_type = true;
+    }
+
     $('#id_course_type').on('keyup keydown blur change', function() {
         if ($("#id_course_type").val() == "") {
             $("#id_course_type").parent().find(".error-text").css("display", "block");
@@ -596,6 +690,38 @@ $(document).ready(function() {
         } else {
             $("#id_course_type").parent().find(".error-text").css("display", "none");
             course_type = true;
+        }
+    });
+
+    if (code == true && name == true && description == true && branch == true && semester == true && faculty == true && course_type == true) {
+        course_btn = true;
+    }
+    else {
+        course_btn = false;
+    }
+    if (course_btn == true) {
+        $("#course-btn").prop("disabled", false);
+        $("input[data-create-design='true']").prop('disabled', false);
+    }
+    else {
+        $("#course-btn").prop("disabled", true);
+        $("input[data-create-design='true']").prop('disabled', true);
+    }
+
+    $('input, select, textarea').on('keyup keydown blur change', function() {
+        if (code == true && name == true && description == true && branch == true && semester == true && faculty == true && course_type == true) {
+            course_btn = true;
+        }
+        else {
+            course_btn = false;
+        }
+        if (course_btn == true) {
+            $("#course-btn").prop("disabled", false);
+            $("input[data-create-design='true']").prop('disabled', false);
+        }
+        else {
+            $("#course-btn").prop("disabled", true);
+            $("input[data-create-design='true']").prop('disabled', true);
         }
     });
 
