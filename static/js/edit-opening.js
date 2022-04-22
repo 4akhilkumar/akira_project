6acde0adaa6062bea79232f5b11da28354129fa5
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-04-22 18:22:48 +0530

    on load validation

--- static/js/edit-opening.js
+++ static/js/edit-opening.js
@@ -25,6 +25,26 @@ $(document).ready(function() {
     else $parent.removeClass("focus");
   });
 
+  if($("#id_job_name").val() == "") {
+    $("#id_job_name").parent().find(".error-text").html("Enter the job name");
+    $("#id_job_name").parent().find(".error-text").css("display", "block");
+    job_name = false;
+  }
+  else if (!$("#id_job_name").val().match(/^[A-Za-z&()\-\s]*$/)) {
+    $("#id_job_name").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the job name correctly&#x0003F;");
+    $("#id_job_name").parent().find(".error-text").css("display", "block");
+    job_name = false;
+  }
+  else if ($("#id_job_name").val().match(/^\s+$/)) {
+    $("#id_job_name").parent().find(".error-text").html("Sorry, but the job name cannot be empty");
+    $("#id_job_name").parent().find(".error-text").css("display", "block");
+    job_name = false;
+  }
+  else {
+    $("#id_job_name").parent().find(".error-text").css("display", "none");
+    job_name = true;
+  }
+
   $('#id_job_name').on('keyup keydown blur change', function() {
     if($("#id_job_name").val() == "") {
       $("#id_job_name").parent().find(".error-text").html("Enter the job name");
@@ -47,6 +67,15 @@ $(document).ready(function() {
     }
   });
 
+  if (!$("#id_job_overview").val().match(/^\s+$/)) {
+    var chars = $('#id_job_overview').val().length;
+    var max = $('#id_job_overview').attr('maxlength');
+    var remaining = max - chars;
+    var char_text = remaining == 1 ? 'character' : 'characters';
+    $('#id_job_overview').parent().find(".info-text").html("You have " + remaining + " " + char_text + " remaining");
+    $('#id_job_overview').parent().find(".info-text").css("display", "block");
+  }
+
   $('#id_job_overview').on('keyup keydown change', function() {
     if (!$("#id_job_overview").val().match(/^\s+$/)) {
       var chars = $('#id_job_overview').val().length;
@@ -57,6 +86,31 @@ $(document).ready(function() {
       $('#id_job_overview').parent().find(".info-text").css("display", "block");
     }
   });
+
+  if($("#id_job_overview").val() == "") {
+    $("#id_job_overview").parent().find(".error-text").html("Enter the job overview");
+    $("#id_job_overview").parent().find(".error-text").css("display", "block");
+    job_overview = false;
+  }
+  else if (!$("#id_job_overview").val().match(/^[A-Za-z0-9-#.,&/\s]*$/)) {
+    $("#id_job_overview").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the job overview correctly&#x0003F;");
+    $("#id_job_overview").parent().find(".error-text").css("display", "block");
+    job_overview = false;
+  }
+  else if ($("#id_job_overview").val().match(/^\s+$/)) {
+    $("#id_job_overview").parent().find(".error-text").html("Sorry, but the job overview cannot be empty");
+    $("#id_job_overview").parent().find(".error-text").css("display", "block");
+    job_overview = false;
+  }        
+  else if ($("#id_job_overview").val().length > 400) {
+    $("#id_job_overview").parent().find(".error-text").html("Sorry, but the job overview cannot be more than 500 characters");
+    $("#id_job_overview").parent().find(".error-text").css("display", "block");
+    job_overview = false;
+  }
+  else {
+    $("#id_job_overview").parent().find(".error-text").css("display", "none");
+    job_overview = true;
+  }
 
   $('#id_job_overview').on('keyup keydown blur change', function() {
     if($("#id_job_overview").val() == "") {
@@ -85,6 +139,15 @@ $(document).ready(function() {
     }
   });
 
+  if (!$("#id_job_description").val().match(/^\s+$/)) {
+    var chars = $('#id_job_description').val().length;
+    var max = $('#id_job_description').attr('maxlength');
+    var remaining = max - chars;
+    var char_text = remaining == 1 ? 'character' : 'characters';
+    $('#id_job_description').parent().find(".info-text").html("You have " + remaining + " " + char_text + " remaining");
+    $('#id_job_description').parent().find(".info-text").css("display", "block");
+  }
+
   $('#id_job_description').on('keyup keydown change', function() {
     if (!$("#id_job_description").val().match(/^\s+$/)) {
       var chars = $('#id_job_description').val().length;
@@ -95,6 +158,31 @@ $(document).ready(function() {
       $('#id_job_description').parent().find(".info-text").css("display", "block");
     }
   });
+
+  if($("#id_job_description").val() == "") {
+    $("#id_job_description").parent().find(".error-text").html("Enter the course description");
+    $("#id_job_description").parent().find(".error-text").css("display", "block");
+    job_desc = false;
+  }
+  else if (!$("#id_job_description").val().match(/^[A-Za-z0-9-#.,&/\s]*$/)) {
+    $("#id_job_description").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the job description correctly&#x0003F;");
+    $("#id_job_description").parent().find(".error-text").css("display", "block");
+    job_desc = false;
+  }
+  else if ($("#id_job_description").val().match(/^\s+$/)) {
+    $("#id_job_description").parent().find(".error-text").html("Sorry, but the job description cannot be empty");
+    $("#id_job_description").parent().find(".error-text").css("display", "block");
+    job_desc = false;
+  }        
+  else if ($("#id_job_description").val().length > 2000) {
+    $("#id_job_description").parent().find(".error-text").html("Sorry, but the job description cannot be more than 2000 characters");
+    $("#id_job_description").parent().find(".error-text").css("display", "block");
+    job_desc = false;
+  }
+  else {
+    $("#id_job_description").parent().find(".error-text").css("display", "none");
+    job_desc = true;
+  }
 
   $('#id_job_description').on('keyup keydown blur change', function() {
     if($("#id_job_description").val() == "") {
@@ -123,6 +211,26 @@ $(document).ready(function() {
     }
   });
 
+  if($("#id_experience").val() == "") {
+    $("#id_experience").parent().find(".error-text").html("Enter the experience");
+    $("#id_experience").parent().find(".error-text").css("display", "block");
+    experience = false;
+  }
+  else if (!$("#id_experience").val().match(/^[A-Za-z0-9\-\s]*$/)) {
+    $("#id_experience").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the experience correctly&#x0003F;");
+    $("#id_experience").parent().find(".error-text").css("display", "block");
+    experience = false;
+  }
+  else if ($("#id_experience").val().match(/^\s+$/)) {
+    $("#id_experience").parent().find(".error-text").html("Sorry, but the experience cannot be empty");
+    $("#id_experience").parent().find(".error-text").css("display", "block");
+    experience = false;
+  }
+  else {
+    $("#id_experience").parent().find(".error-text").css("display", "none");
+    experience = true;
+  }
+
   $('#id_experience').on('keyup keydown blur change', function() {
     if($("#id_experience").val() == "") {
       $("#id_experience").parent().find(".error-text").html("Enter the experience");
@@ -144,6 +252,26 @@ $(document).ready(function() {
       experience = true;
     }
   });
+
+  if($("#id_qualification").val() == "") {
+    $("#id_qualification").parent().find(".error-text").html("Enter the qualification");
+    $("#id_qualification").parent().find(".error-text").css("display", "block");
+    qualification = false;
+  }
+  else if (!$("#id_qualification").val().match(/^[A-Za-z./\-\s]*$/)) {
+    $("#id_qualification").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the qualification correctly&#x0003F;");
+    $("#id_qualification").parent().find(".error-text").css("display", "block");
+    qualification = false;
+  }
+  else if ($("#id_qualification").val().match(/^\s+$/)) {
+    $("#id_qualification").parent().find(".error-text").html("Sorry, but the qualification cannot be empty");
+    $("#id_qualification").parent().find(".error-text").css("display", "block");
+    qualification = false;
+  }
+  else {
+    $("#id_qualification").parent().find(".error-text").css("display", "none");
+    qualification = true;
+  }
 
   $('#id_qualification').on('keyup keydown blur change', function() {
     if($("#id_qualification").val() == "") {
@@ -167,6 +295,26 @@ $(document).ready(function() {
     }
   });
 
+  if($("#id_location").val() == "") {
+    $("#id_location").parent().find(".error-text").html("Enter the location");
+    $("#id_location").parent().find(".error-text").css("display", "block");
+    location = false;
+  }
+  else if (!$("#id_location").val().match(/^[A-Za-z\-\s]*$/)) {
+    $("#id_location").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the location correctly&#x0003F;");
+    $("#id_location").parent().find(".error-text").css("display", "block");
+    location = false;
+  }
+  else if ($("#id_location").val().match(/^\s+$/)) {
+    $("#id_location").parent().find(".error-text").html("Sorry, but the location cannot be empty");
+    $("#id_location").parent().find(".error-text").css("display", "block");
+    location = false;
+  }
+  else {
+    $("#id_location").parent().find(".error-text").css("display", "none");
+    location = true;
+  }
+
   $('#id_location').on('keyup keydown blur change', function() {
     if($("#id_location").val() == "") {
       $("#id_location").parent().find(".error-text").html("Enter the location");
@@ -188,6 +336,26 @@ $(document).ready(function() {
       location = true;
     }
   });
+
+  if($("#id_pay_scale").val() == "") {
+    $("#id_pay_scale").parent().find(".error-text").html("Enter the pay scale");
+    $("#id_pay_scale").parent().find(".error-text").css("display", "block");
+    pay_scale = false;
+  }
+  else if (!$("#id_pay_scale").val().match(/^[A-Za-z0-9\-\s]*$/)) {
+    $("#id_pay_scale").parent().find(".error-text").html("Are you sure that you&#x00027;ve entered the pay scale correctly&#x0003F;");
+    $("#id_pay_scale").parent().find(".error-text").css("display", "block");
+    pay_scale = false;
+  }
+  else if ($("#id_pay_scale").val().match(/^\s+$/)) {
+    $("#id_pay_scale").parent().find(".error-text").html("Sorry, but the pay scale cannot be empty");
+    $("#id_pay_scale").parent().find(".error-text").css("display", "block");
+    pay_scale = false;
+  }
+  else {
+    $("#id_pay_scale").parent().find(".error-text").css("display", "none");
+    pay_scale = true;
+  }
 
   $('#id_pay_scale').on('keyup keydown blur change', function() {
     if($("#id_pay_scale").val() == "") {
@@ -211,6 +379,14 @@ $(document).ready(function() {
     }
   });
 
+  if ($("#id_job_type").val() == "") {
+    $("#id_job_type").parent().find(".error-text").css("display", "block");
+    job_type = false;
+  } else {
+    $("#id_job_type").parent().find(".error-text").css("display", "none");
+    job_type = true;
+  }
+
   $('#id_job_type').on('keyup keydown blur change', function() {
     if ($("#id_job_type").val() == "") {
       $("#id_job_type").parent().find(".error-text").css("display", "block");
@@ -220,6 +396,14 @@ $(document).ready(function() {
       job_type = true;
     }
   });
+
+  if ($("#id_contact_person").val() == "") {
+    $("#id_contact_person").parent().find(".error-text").css("display", "block");
+    contact_person = false;
+  } else {
+    $("#id_contact_person").parent().find(".error-text").css("display", "none");
+    contact_person = true;
+  }
 
   $('#id_contact_person').on('keyup keydown blur change', function() {
     if ($("#id_contact_person").val() == "") {
@@ -231,21 +415,47 @@ $(document).ready(function() {
     }
   });
 
-  // $('input, select, textarea').on('keyup keydown blur change', function() {
-  //   if (job_name == true && job_overview == true && job_desc == true && experience == true && qualification == true && location == true && pay_scale == true && job_type == true && contact_person == true) {
-  //     opening_btn = true;
-  //   }
-  //   else {
-  //     opening_btn = false;
-  //   }
-  //   if (opening_btn == true) {
-  //     $("#edit_opening-btn").prop("disabled", false);
-  //   }
-  //   else {
-  //     $("#edit_opening-btn").prop("disabled", true);
-  //   }
-  // });
+  if (job_name == true && job_overview == true && job_desc == true && experience == true && qualification == true && location == true && pay_scale == true && job_type == true && contact_person == true) {
+    opening_btn = true;
+  }
+  else {
+    opening_btn = false;
+  }
+  if (opening_btn == true) {
+    $("#edit_opening-btn").prop("disabled", false);
+  }
+  else {
+    $("#edit_opening-btn").prop("disabled", true);
+  }
 
+  $('input, select, textarea').on('keyup keydown blur change', function() {
+    if (job_name == true && job_overview == true && job_desc == true && experience == true && qualification == true && location == true && pay_scale == true && job_type == true && contact_person == true) {
+      opening_btn = true;
+    }
+    else {
+      opening_btn = false;
+    }
+    if (opening_btn == true) {
+      $("#edit_opening-btn").prop("disabled", false);
+    }
+    else {
+      $("#edit_opening-btn").prop("disabled", true);
+    }
+  });
+
+});
+
+$("input[type=text], input[type=number]").each(function () {
+  var $input = $(this);
+  var $parent = $input.closest(".input-box");
+  if ($input.val()) $parent.addClass("focus");
+   else $parent.removeClass("focus");
+});
+$("textarea").each(function () {
+  var $input = $(this);
+  var $parent = $input.closest(".input-box");
+  if ($input.val()) $parent.addClass("focus");
+      else $parent.removeClass("focus");
 });
 
 function setFocus(on) {
