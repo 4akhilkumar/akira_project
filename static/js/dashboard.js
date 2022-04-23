2faf01ab701962acc34f568248ebd0c3f7f72681
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-04-23 22:50:49 +0530

    color theme

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,52 +1,43 @@
-/*====================== Dark Mode =====================*/
-// check for saved 'darkMode' in localStorage
-let darkMode = localStorage.getItem('darkMode'); 
-const darkModeToggle = document.querySelector('#dark-mode-toggle');
-const themeToggle = document.querySelector('#theme');
+/* ====================== Dark Mode ===================== */
+const themeBtn = document.querySelector('.theme');
 
-const enableDarkMode = () => {
-// 1. Add the class to the body
-document.body.classList.add('darkmode');
-// 2. Add the class to the body
-themeToggle.classList.add('darkmode-active');
-themeToggle.classList.remove('lightmode-active');
-// 3. Update darkMode in localStorage
-localStorage.setItem('darkMode', 'enabled');
+function getCurrentTheme(){
+  let theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  localStorage.getItem('chosenTheme') ? theme = localStorage.getItem('chosenTheme') : null;
+  return theme;
 }
 
-const disableDarkMode = () => {
-// 1. Remove the class from the body
-document.body.classList.remove('darkmode');
-// 2. Remove the class from the body
-themeToggle.classList.remove('darkmode-active');
-themeToggle.classList.add('lightmode-active');
-// 3. Update darkMode in localStorage 
-localStorage.setItem('darkMode', null);
+function loadTheme(theme){
+  const root = document.querySelector(':root');
+  if(theme === "light"){
+    themeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" class="bkg2--stroke" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`;
+  } else {
+    themeBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" class="bkg2--stroke" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`;
+  }
+  root.setAttribute('color-scheme', `${theme}`);
 }
 
-// If the user already visited and enabled darkMode
-// start things off with it on
-if (darkMode === 'enabled') {
-  enableDarkMode();
-}
-else {
-  disableDarkMode();
-}
+themeBtn.addEventListener('click', () => {
+  let theme = getCurrentTheme();
+  let audio;
+  if(theme === 'dark'){
+    audio = document.querySelector('.theme-audio--light-on');
+    theme = 'light';
+  } else {
+    audio = document.querySelector('.theme-audio--light-off');
+    theme = 'dark';
+  }
+  audio.currentTime = 0;
+  audio.play();
+  localStorage.setItem('chosenTheme', `${theme}`);
+  loadTheme(theme);
+})
 
-// When someone clicks the button
-darkModeToggle.addEventListener('click', () => {
-// get their darkMode setting
-darkMode = localStorage.getItem('darkMode'); 
+window.addEventListener('DOMContentLoaded', () => {
+  loadTheme(getCurrentTheme());
+})
 
-// if it not current enabled, enable it
-if (darkMode !== 'enabled') {
-    enableDarkMode();
-// if it has been enabled, turn it off  
-} else {  
-    disableDarkMode(); 
-}
-});
-// ====================== End Dark Mode ====================== //
+/* ====================== End Dark Mode ====================== */
 
 // ====================== Top Button ====================== //
 var mybutton = document.getElementById("myTopButton");
