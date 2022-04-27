dab1569d168816d0747eb0fdc2184ea08d01249d
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-04-27 14:59:33 +0530

    Bug Fixed
    'URLShortenerMC' object has no attribute 'short_url'

--- akira_apps/URLShortener/models.py
+++ akira_apps/URLShortener/models.py
@@ -14,7 +14,7 @@ class URLShortenerMC(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return '%s %s' % (self.user, self.short_url)
+        return '%s %s' % (self.user, self.long_url_path)
     
     class Meta:
         ordering = ["user__username", "created_at"]
