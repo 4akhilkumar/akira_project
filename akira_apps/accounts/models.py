16740644ab8b16bc48dc7b0bf241d03cccfc7f4d
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:30:14 +0530

    field name changed
    created_at -> changed_on

--- akira_apps/accounts/models.py
+++ akira_apps/accounts/models.py
@@ -7,10 +7,10 @@ class TwoFactorAuth(models.Model):
     id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(User, unique=True, on_delete = models.CASCADE)
     twofa = models.BooleanField(default = False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    changed_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '%s' % (self.user)
 
     class Meta: 
-        ordering = ['created_at']
+        ordering = ['changed_on']
