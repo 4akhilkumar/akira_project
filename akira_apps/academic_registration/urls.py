94c14d5c35bf80b2f50ab230ef5f00e361956245
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:29:54 +0530

    aca_Registration, branch, Semester URLs

--- akira_apps/academic_registration/urls.py
+++ akira_apps/academic_registration/urls.py
@@ -2,14 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sem_registration/', views.sem_registration, name='sem_registration'),
-    path('enrollSpec/<uuid:speci_id>/', views.enrollSpec, name='enrollSpec'),
-    path('unenrollSpec/<uuid:speci_id>/', views.unenrollSpec, name='unenrollSpec'),
-    path('create_semester_save/', views.create_semester_save, name='create_semester_save'),
-    path('fetch_semester/<semester_id>/', views.fetch_semester, name='fetch_semester'),
-    path('update_semester_save/<semester_id>/', views.update_semester_save, name='update_semester_save'),
-    path('delete_semester/<semester_id>/', views.delete_semester, name='delete_semester'),
+    path('aca_Registration/', views.aca_Registration, name='aca_Registration'),
 
+    path('createsemester/', views.createsemester, name='createsemester'),
     path('createsemesterAjax/', views.createsemesterAjax, name='createsemesterAjax'),
-    path('getAllSemestersAjax/', views.getAllSemestersAjax, name='getAllSemestersAjax')
+    path('getAllSemestersAjax/', views.getAllSemestersAjax, name='getAllSemestersAjax'),
+
+    path('setTeachingStaffSemesterRegistrationAjax/', views.setTeachingStaffSemesterRegistrationAjax, name='setTeachingStaffSemesterRegistrationAjax'),
+    path('setStudentSemesterRegistrationAjax/', views.setStudentSemesterRegistrationAjax, name='setStudentSemesterRegistrationAjax'),
+    
+    path('createbranchAjax/', views.createbranchAjax, name='createbranchAjax'),
+    path('createbranch/', views.createbranch, name='createbranch'),
+    path('getAllBranchesAjax/', views.getAllBranchesAjax, name='getAllBranchesAjax'),
+    path('studentAcaReg/', views.studentAcaReg, name='studentAcaReg'),
 ]
--- akira_apps/academic_registration/views.py
+++ akira_apps/academic_registration/views.py
@@ -1,17 +1,66 @@
 from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.http import JsonResponse
+from akira_apps.adops.models import AdmissionRegister
 
-from akira_apps.super_admin.decorators import allowed_users
+from akira_apps.super_admin.decorators import (allowed_users)
 from akira_apps.academic.models import (Semester, Branch)
 from akira_apps.academic.forms import (SemesterModeForm)
-from akira_apps.specialization.models import (SpecializationsMC)
-from akira_apps.academic_registration.models import (SpecEnrollStudent)
+from akira_apps.academic_registration.models import (SetSemesterRegistration)
+
+def aca_Registration(request):
+    branches = Branch.objects.all()
+    semesters = Semester.objects.all().order_by('-is_active')
+    semesterModeForm = SemesterModeForm()
+    getActiveSemesters = Semester.objects.filter(is_active=True)
+    context = {
+        "branches": branches,
+        "semesters":semesters,
+        "semesterModeForm":semesterModeForm,
+        "getActiveSemesters":getActiveSemesters,
+    }
+    return render(request, 'academic_registration/acaRegistration.html', context)
+
+def getAllBranchesAjax(request):
+    getBranches = Branch.objects.all()
+    return JsonResponse(list(getBranches.values('id', 'name')), safe = False)
+
+@allowed_users(allowed_roles=['Administrator', 'Teaching Staff', 'Adops Team'])
+def createbranchAjax(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        if Branch.objects.filter(name__contains = name).exists() is False:
+            Branch.objects.create(name = name, description = desc)
+            message = "Branch %s created successfully!" % str(name)
+            status = "success"
+        else:
+            message = "Branch %s already exists!" % str(name)
+            status = "failed"
+        return JsonResponse({
+            'message': message,
+            'status': status
+            }, safe = False)
+    else:
+        return JsonResponse({'message': "Invalid request"}, safe = False)
+
+@allowed_users(allowed_roles=['Administrator', 'Teaching Staff', 'Adops Team'])
+def createbranch(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        desc = request.POST.get('desc')
+        if Branch.objects.filter(name__contains = name).exists() is False:
+            Branch.objects.create(name = name, description = desc)
+            messages.success(request, "Branch %s created successfully!" % str(name))
+        else:
+            messages.info(request, "Branch %s already exists!" % str(name))
+    return redirect('aca_Registration')
 
 def getAllSemestersAjax(request):
     getSemesters = Semester.objects.all()
-    return JsonResponse(list(getSemesters.values('id', 'mode', 'start_year', 'end_year', 'branch', 'is_active')), safe = False)
+    return JsonResponse(list(getSemesters.values('id', 'mode', 'start_year', 'end_year', 'branch__name', 'is_active')), safe = False)
 
+@allowed_users(allowed_roles=['Administrator', 'Teaching Staff'])
 def createsemesterAjax(request):
     if request.method == "POST":
         semesterMode = request.POST.get('mode')
@@ -44,98 +93,91 @@ def createsemesterAjax(request):
     else:
         return JsonResponse({'message': "Invalid request"}, safe = False)
 
-@allowed_users(allowed_roles=['Administrator', 'Head of the Department'])
-def create_semester_save(request):
-    if request.method == 'POST':
+@allowed_users(allowed_roles=['Administrator', 'Teaching Staff'])
+def createsemester(request):
+    if request.method == "POST":
         semesterMode = request.POST.get('semester_mode')
-        semesterStartYear = request.POST.get('semester_start_year')
-        semesterEndYear = request.POST.get('semester_end_year')
+        semesterStartYear = request.POST.get('start_year')
+        semesterEndYear = request.POST.get('end_year')
         semesterBranch = request.POST.get('branch')
-        semesterisActive = request.POST.get('semester_is_active')
-        if semesterisActive == 'on':
-            semesterisActive = True
-        else:
-            semesterisActive = False
-        createSemester = Semester.objects.create(mode=semesterMode,
-                                                start_year=semesterStartYear,
-                                                end_year=semesterEndYear,
-                                                branch = semesterBranch,
-                                                is_active=semesterisActive)
-        createSemester.save()
-        return redirect('sem_registration')
-
-@allowed_users(allowed_roles=['Administrator', 'Head of the Department'])
-def fetch_semester(request, semester_id):
-    semester = Semester.objects.get(id=semester_id)
-    context = {
-        'semester': semester
-    }
-    return render(request, 'academic/semester/edit_semester.html', context)
-
-@allowed_users(allowed_roles=['Administrator', 'Head of the Department'])
-def update_semester_save(request, semester_id):
-    if request.method == 'POST':
-        semesterMode = request.POST.get('semester_mode')
-        semesterStartYear = request.POST.get('semester_start_year')
-        semesterEndYear = request.POST.get('semester_end_year')
-        semesterisActive = request.POST.get('semester_is_active')
-        if semesterisActive == 'on':
-            semesterisActive = True
-        else:
-            semesterisActive = False
-        updateSemester = Semester.objects.get(id = semester_id)
-        updateSemester.mode=semesterMode
-        updateSemester.start_year=semesterStartYear
-        updateSemester.end_year=semesterEndYear
-        updateSemester.is_active=semesterisActive
-        updateSemester.save()
-        return redirect('sem_registration')
-
-@allowed_users(allowed_roles=['Administrator', 'Head of the Department'])
-def delete_semester(request, semester_id):
-    semester = Semester.objects.get(id = semester_id)
-    semester.delete()
-    return redirect('sem_registration')
-
-def enrollSpec(request, speci_id):
-    if request.method == "POST":
-        getSpecObj = SpecializationsMC.objects.get(id=speci_id)
-        if getSpecObj.capacity != 0:
-            try:
-                SpecEnrollStudent.objects.create(user = request.user, enrolledSpec = getSpecObj)
-                getSpecObj.capacity -= 1
-                getSpecObj.save()
-                messages.success(request, "You have enrolled successfully")
-            except Exception:
-                messages.info(request, "You have already enrolled")
-        else:
-            messages.info(request, "Sorry, Enroll for %s is Closed" % (getSpecObj.specialization_name))
-    return redirect('manage_specializations')
-
-def unenrollSpec(request, speci_id):
-    if request.method == "POST":
-        getSpecObj = SpecializationsMC.objects.get(id=speci_id)
         try:
-            SpecEnrollStudent.objects.get(user = request.user, enrolledSpec = getSpecObj).delete()
-            getSpecObj.capacity += 1
-            getSpecObj.save()
-            messages.success(request, "You have Unenrolled successfully")
-        except Exception:
-            messages.info(request, "You have already Unenrolled")
-    return redirect('manage_specializations')
+            semesterBranch = Branch.objects.get(id=semesterBranch)
+        except Branch.DoesNotExist:
+            messages.error(request, "Branch does not exist!")
+            messages.info(request, "Please create a branch")
+            return redirect('manage_academic')
+        semesterisActive = request.POST.get('semester_is_active')
+        if semesterisActive == 'on':
+            semesterisActive = True 
+        else:
+            semesterisActive = False
+        if Semester.objects.filter(mode=semesterMode, start_year=semesterStartYear, end_year=semesterEndYear, branch = semesterBranch).exists() is False:
+            Semester.objects.create(mode=semesterMode, start_year=semesterStartYear,
+                                    end_year=semesterEndYear, branch = semesterBranch,
+                                    is_active=semesterisActive)
+            messages.success(request, "Semester created successfully!")
+            return redirect('aca_Registration')
+        else:
+            messages.info(request, "{} Semester {} already exists!".format(str(semesterMode), str(semesterStartYear)))
+    return redirect('aca_Registration')
 
-def sem_registration(request):
-    branch_list = Branch.objects.all()
-    semesters = Semester.objects.all()
-    semesterModeForm = SemesterModeForm()
-    getActiveSemester = Semester.objects.filter(is_active=True)
-    activeSemesterMode = "--"
-    if getActiveSemester:
-        activeSemesterMode = getActiveSemester[0].mode
+
+def setTeachingStaffSemesterRegistrationAjax(request):
+    if request.method == "POST":
+        semesterId = request.POST.get('semester_id')
+        if Semester.objects.filter(id=semesterId).exists() is True:
+            semesterObj = Semester.objects.get(id=semesterId)
+            try:
+                getSemesterStatus = SetSemesterRegistration.objects.get(semester = semesterObj)
+            except SetSemesterRegistration.DoesNotExist:
+                getSemesterStatus = SetSemesterRegistration.objects.create(semester = semesterObj, teachingstaff = True)
+                message = "Semester registration status set successfully!"
+                status = "success"
+            if getSemesterStatus.teachingstaff is True:
+                getSemesterStatus.teachingstaff = False
+                getSemesterStatus.save()
+                message = "Semester registration status changed successfully!"
+                status = "success"
+            else:
+                getSemesterStatus.teachingstaff = True
+                getSemesterStatus.save()
+                message = "Semester registration status changed successfully!"
+                status = "success"
+        else:
+            message = "Semester does not exist!"
+            status = "error"
+        return JsonResponse({'message': message, 'status': status}, safe = False)
+
+def setStudentSemesterRegistrationAjax(request):
+    if request.method == "POST":
+        semesterId = request.POST.get('semester_id')
+        if Semester.objects.filter(id=semesterId).exists() is True:
+            semesterObj = Semester.objects.get(id=semesterId)
+            try:
+                getSemesterStatus = SetSemesterRegistration.objects.get(semester = semesterObj)
+            except SetSemesterRegistration.DoesNotExist:
+                getSemesterStatus = SetSemesterRegistration.objects.create(semester = semesterObj, students = True)
+                message = "Semester registration status set successfully!"
+                status = "success"
+            if getSemesterStatus.students is True:
+                getSemesterStatus.students = False
+                getSemesterStatus.save()
+                message = "Semester registration status changed successfully!"
+                status = "success"
+            else:
+                getSemesterStatus.students = True
+                getSemesterStatus.save()
+                message = "Semester registration status changed successfully!"
+                status = "success"
+        else:
+            message = "Semester does not exist!"
+            status = "error"
+        return JsonResponse({'message': message, 'status': status}, safe = False)
+
+def studentAcaReg(request):
+    if AdmissionRegister.objects.filter(user = request.user).exists() is True:
+        admissionInfo = AdmissionRegister.objects.get(user = request.user)
     context = {
-        "branch_list":branch_list,
-        "semesters":semesters,
-        "semesterModeForm":semesterModeForm,
-        "activeSemesterMode":activeSemesterMode,
+        "admissionInfo": admissionInfo,
     }
-    return render(request, 'academic_registration/semRegistration.html', context)
+    return render(request, 'academic_registration/studentAcademyReg.html', context)
