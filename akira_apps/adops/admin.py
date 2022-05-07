4961f59ddc852087a34fccf629c3e194133b02d1
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:30:50 +0530

    model classes unregistered

--- akira_apps/adops/admin.py
+++ akira_apps/adops/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import (Openings, ExtraFieldOpenings)
 
-admin.site.register(Openings)
-admin.site.register(ExtraFieldOpenings)
+# Register your models here.
