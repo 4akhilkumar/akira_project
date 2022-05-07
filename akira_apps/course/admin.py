39f411e33f3919fea6c214f5cee47816efbe2777
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 15:39:15 +0530

    model classes unregistered

--- akira_apps/course/admin.py
+++ akira_apps/course/admin.py
@@ -1,15 +1,3 @@
 from django.contrib import admin
-from .models import (CourseMC, CourseFiles,
-                    CourseComponent, CourseSubComponent,
-                    TaskAnswer, CourseTask, CourseOfferingType, 
-                    CourseCOTExtraFields, CourseExtraFields)
 
-admin.site.register(CourseMC)
-admin.site.register(CourseFiles)
-admin.site.register(CourseComponent)
-admin.site.register(CourseSubComponent)
-admin.site.register(CourseCOTExtraFields)
-admin.site.register(TaskAnswer)
-admin.site.register(CourseTask)
-admin.site.register(CourseOfferingType)
-admin.site.register(CourseExtraFields)
+# Register your models here.
