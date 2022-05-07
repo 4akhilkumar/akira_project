9e3d512c6d555f9901746bf1ca1f91ad13c86ea7
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:26:44 +0530

    new model classes added
    Branch, Semester, DesignCoursesSemester, SetSemesterRegistration, TeachingStaffSemRegistration, StudentSemRegistration

--- akira_apps/academic_registration/models.py
+++ akira_apps/academic_registration/models.py
@@ -3,48 +3,69 @@ from django.db import models
 
 import uuid
 
-from akira_apps.specialization.models import (SpecializationsMC)
+from akira_apps.course.models import (CourseMC)
 
-class SpecEnrollStudent(models.Model):
+class Branch(models.Model):
     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-    user = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
-    enrolledSpec = models.ForeignKey(SpecializationsMC, on_delete = models.SET_NULL, blank = True, null = True)
+    name = models.CharField(max_length = 50, unique = True)
+    description = models.CharField(max_length = 500, blank = True, null = True)
 
     def __str__(self):
-        return '%s - %s' % (self.user.username, self.enrolledSpec)
+        return '%s' % (self.name)
+    
+    class Meta:
+        ordering = ['name']
+
+class Semester(models.Model):
+    MODE = [
+        ('ODD','ODD'),
+        ('EVEN','EVEN'),
+    ]
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    mode = models.CharField(max_length = 4, choices = MODE, default = 1)
+    start_year = models.DateField()
+    end_year = models.DateField()
+    branch = models.ForeignKey(Branch, on_delete=models.SET_NULL, blank=True, null=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return '%s %s - %s' % (self.mode, self.start_year.year, self.branch.name)
 
     class Meta:
-        unique_together = ['user','enrolledSpec']
+        ordering = ['-start_year']
 
+class DesignCoursesSemester(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    semester = models.OneToOneField(Semester, on_delete=models.CASCADE)
+    course = models.ManyToManyField(CourseMC)
 
-# class academic_registration_staff(models.Model):
-#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-#     staff = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True,)
-#     branch = models.CharField(max_length = 50, unique = True, choices = BRANCH_CHOICES, default=1)
+class SetSemesterRegistration(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    semester = models.OneToOneField(Semester, on_delete = models.CASCADE)
+    teachingstaff = models.BooleanField(default = False)
+    students = models.BooleanField(default = False)
 
-# class academic_registration_student(models.Model):
-#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-#     student = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
-#     branch = models.CharField(max_length = 50, unique = True, choices = BRANCH_CHOICES, default=1)
+    def __str__(self):
+        return '%s - %s - %s ' % (self.semester, self.teachingstaff, self.students)
 
-# class specialization_registration_staff(models.Model):
-#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-#     staff = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
-#     specialization = models.ForeignKey(Specialization, on_delete = models.SET_NULL, blank = True, null = True)
+class TeachingStaffSemRegistration(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    semester = models.ForeignKey(Semester, on_delete = models.CASCADE)
+    teachingStaff = models.ForeignKey(User, on_delete = models.CASCADE)
 
-# class specialization_registration_student(models.Model):
-#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-#     student = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
-#     specialization = models.ForeignKey(Specialization, on_delete = models.SET_NULL, blank = True, null = True)
+    def __str__(self):
+        return '%s - %s ' % (self.semester, self.staff)
 
-#     class Meta:
-#         unique_together = ('student','specialization')
+    class Meta:
+        unique_together = ['semester','teachingStaff']
 
-# class course_registration_staff(models.Model):
-#     id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
-#     staff = models.ForeignKey(User, on_delete = models.SET_NULL, blank = True, null = True)
-#     course = models.ForeignKey(Course, on_delete=models.SET_NULL, blank=True, null=True)
-#     section = models.ForeignKey(SectionRooms, on_delete=models.SET_NULL, blank=True, null=True)
+class StudentSemRegistration(models.Model):
+    id = models.UUIDField(primary_key = True, unique = True, default = uuid.uuid4, editable = False)
+    semester = models.ForeignKey(Semester, on_delete = models.CASCADE)
+    student = models.ForeignKey(User, on_delete = models.CASCADE)
 
-#     class Meta:
-#         unique_together = ('course','section')
+    def __str__(self):
+        return '%s - %s ' % (self.semester, self.student)
+
+    class Meta:
+        unique_together = ['semester','student']
