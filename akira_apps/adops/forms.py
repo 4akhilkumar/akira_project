4c8f067199b510b61446559b952c6db0a1ff436f
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:31:52 +0530

    ExtraFieldValueTypeForm form removed

--- akira_apps/adops/forms.py
+++ akira_apps/adops/forms.py
@@ -1,8 +1,5 @@
 from django import forms
-from .models import (JOB_TYPE_CHOICES, FIELD_VALUE_TYPE)
+from .models import (JOB_TYPE_CHOICES)
 
 class OpeningsJobTypeForm(forms.Form):
     job_type = forms.ChoiceField(choices = JOB_TYPE_CHOICES)
-
-class ExtraFieldValueTypeForm(forms.Form):
-    field_value_type = forms.ChoiceField(choices = FIELD_VALUE_TYPE)
