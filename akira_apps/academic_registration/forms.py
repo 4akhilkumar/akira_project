4a3f217c8439d0117913257da0564482657e1097
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-07 14:25:50 +0530

    SectionRoomForm removed and SemesterModeForm added

--- akira_apps/academic_registration/forms.py
+++ akira_apps/academic_registration/forms.py
@@ -1,14 +1,8 @@
 from django import forms
-from django.db import models
 
-class SectionRoomForm(forms.Form):
-    class Floor(models.TextChoices):
-        Ground = 'Ground'
-        First = 'First'
-        Second = 'Second'
-        Third = 'Third'
-        Fourth = 'Fourth'
-        Fifth = 'Fifth'
-        Sixth = 'Sixth'
-    floor = forms.ChoiceField(choices = Floor.choices)
-    fields = ['floor']
+class SemesterModeForm(forms.Form):
+    MODE = [
+        ('ODD','ODD'),
+        ('EVEN','EVEN'),
+    ]
+    semesterModeForm = forms.ChoiceField(choices = MODE)
