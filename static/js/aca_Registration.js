492b56ec24b099c5a55a78b408694e2fdf65a8f4
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-05-28 21:21:09 +0530

    load function and table - sortable and searchable functions added

--- static/js/aca_Registration.js
+++ static/js/aca_Registration.js
@@ -225,7 +225,14 @@ $(document).on('click', '.semRegStatus', function() {
             'semester_id': semester_id
         },
         success: function (data) {
-            
+            if (data.status == "success") {
+                toastr.success(data.message);
+                $("#id_acaRegTable").load(location.href + " #id_acaRegTable", "");
+            }
+            else {
+                toastr.error(data.message);
+                $("#id_acaRegTable").load(location.href + " #id_acaRegTable", "");
+            }
         },
         error: function (data) {
             console.log("Something went wrong");
@@ -245,17 +252,84 @@ $(document).on('click', '.semester_status', function() {
             'semester_id': semester_id
         },
         success: function (data) {
-            console.log(data);
             if (data.status == "success") {
                 toastr.success(data.message);
                 $("#id_active_semesters").load(location.href + " #id_active_semesters", "");
+                $("#id_acaRegTable").load(location.href + " #id_acaRegTable", "");
             }
             else {
                 toastr.error(data.message);
+                $("#id_acaRegTable").load(location.href + " #id_acaRegTable", "");
             }
         },
         error: function (data) {
             console.log("Something went wrong");
         }
+    });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelectorAll(".search-input").forEach((inputField) => {
+    const tableRows = inputField
+      .closest("table")
+      .querySelectorAll("tbody > tr");
+    const headerCell = inputField.closest("th");
+    const otherHeaderCells = headerCell.closest("tr").children;
+    const columnIndex = Array.from(otherHeaderCells).indexOf(headerCell);
+    const searchableCells = Array.from(tableRows).map(
+      (row) => row.querySelectorAll("td")[columnIndex]
+    );
+
+    inputField.addEventListener("input", () => {
+      const searchQuery = inputField.value.toLowerCase();
+
+      for (const tableCell of searchableCells) {
+        const row = tableCell.closest("tr");
+        const value = tableCell.textContent.toLowerCase().replace(",", "");
+
+        row.style.visibility = null;
+
+        if (value.search(searchQuery) === -1) {
+          row.style.visibility = "collapse";
+        }
+      }
+    });
+  });
+});
+
+function sortTableByColumn(table, column, asc = true) {
+    const dirModifier = asc ? 1 : -1;
+    const tBody = table.tBodies[0];
+    const rows = Array.from(tBody.querySelectorAll("tr"));
+
+    // Sort each row
+    const sortedRows = rows.sort((a, b) => {
+        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();
+
+        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);
+    });
+
+    // Remove all existing TRs from the table
+    while (tBody.firstChild) {
+        tBody.removeChild(tBody.firstChild);
+    }
+
+    // Re-add the newly sorted rows
+    tBody.append(...sortedRows);
+
+    // Remember how the column is currently sorted
+    table.querySelectorAll("th").forEach(th => th.classList.remove("th-sort-asc", "th-sort-desc"));
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-asc", asc);
+    table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle("th-sort-desc", !asc);
+}
+
+document.querySelectorAll(".table-sortable .table-header-title").forEach(headerCell => {
+    headerCell.addEventListener("click", () => {
+        const tableElement = headerCell.parentElement.parentElement.parentElement;
+        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);
+        const currentIsAscending = headerCell.classList.contains("th-sort-asc");
+
+        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);
     });
 });
