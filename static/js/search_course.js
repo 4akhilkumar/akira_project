cf1132f3d4e013f7502e05da6fc31e9def5d585f
4akhilkumar <72900242+4akhilkumar@users.noreply.github.com>
2022-06-08 01:15:59 +0530

    ID -> Class

--- static/js/search_course.js
+++ static/js/search_course.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-    var createCourseUrl = $('#createCourse').data('create-course-url');
-    $('#createCourse').click(function() {
-        window.location.href = createCourseUrl;
-    });
-
     var placeholderArray = ['Course Name', 'Course Code', 'Anything related to it!'];
     setInterval(function() {
         var searchQueryInput = $('.searchQueryInput').attr('placeholder', "Search " + placeholderArray[Math.floor(Math.random() * placeholderArray.length)]);
@@ -46,23 +41,4 @@ $('input').on('keyup keydown blur change', function() {
     else {
         $("#searchQuerySubmit").prop("disabled", true);
     }
-});
-
-
-var bulkCoursesCreationModal = document.getElementById("bulkCoursesCreationModalForm");
-var showFormBulkCourses = document.getElementById("createCourseBulk");
-var bulkCoursesCreationSpan = document.getElementById("close-bulkCoursesCreationModal");
-
-showFormBulkCourses.onclick = function() {
-  bulkCoursesCreationModal.style.display = "block";
-}
-
-bulkCoursesCreationSpan.onclick = function() {
-  bulkCoursesCreationModal.style.display = "none";
-}
-
-$(window).click(function(event) {
-  if (event.target == bulkCoursesCreationModal) {
-    bulkCoursesCreationModal.style.display = "none";
-  }
 });
